16b06537d7a816aa54dbb39d2ea378aa7519de63
Niels27 <nielsvdwal.27@gmail.com>
2023-04-11 15:41:09 +0200

    finishedd

--- src/app/boggle/boggle.component.ts
+++ src/app/boggle/boggle.component.ts
@@ -6,18 +6,34 @@ import { BoggleBoard } from '../boggleboard'
 
 
 
+
 @Component({
   selector: 'app-boggle',
   templateUrl: './boggle.component.html',
-  styleUrls: ['./boggle.component.css']
+  styleUrls: ['./boggle.component.css'],
+ 
 })
 export class BoggleComponent implements OnInit {
+  words: string[] = [];
+  timer: string = '3:00';
+  timerOn: boolean=false;
   dice: Die[][] = [];
   id: string = '';
   letters: string = '';
   score: number = 0;
-  words: string[] = [];
+  scores: string[] = [];
+  playerName : string|null="";
 
+  async onStart() {
+    this.boggleService.getBoggleBoard().subscribe(
+      (boggleBoard) => (this.dice = boggleBoard.dice, this.id = boggleBoard.boggleBoardId));
+    this.startTimer(10)
+  }
+  async onReset() {
+    console.log("reset");
+    this.letters = '';
+    this.clearService.clear();
+  }
   async onSubmit() {
     let isValid = await this.boggleService.getIsValid(this.id, this.letters);
     if (isValid) {
@@ -28,12 +44,11 @@ export class BoggleComponent implements OnInit {
         this.words.push(this.letters);
         console.log(this.score);
       }
-    }
-
+    }else{alert("Word not valid!")}
     this.letters = '';
     this.clearService.clear();
-
   }
+
 
   addLetter(letter: string) {
     this.letters = this.letters.concat(letter);
@@ -42,10 +57,32 @@ export class BoggleComponent implements OnInit {
   constructor(private boggleService: BoggleService, private clearService: ClearService) { }
 
   ngOnInit(): void {
-    this.boggleService.getBoggleBoard().subscribe(
-      (boggleBoard) => (this.dice = boggleBoard.dice, this.id = boggleBoard.boggleBoardId));
+
   };
 
+  startTimer(totalSeconds: number) {
+    this.timerOn = true;
+    const countdown = setInterval(() => {
+     
+      if (totalSeconds === 0) {
+        if (this.timerOn) {
+          alert('Time is up!')
+          this.playerName = prompt("Please enter your name");
+          if(this.playerName==null||undefined){this.playerName="anonymous"}
+          this.timerOn = false;
+          this.letters = '';
+          this.scores.push(this.playerName+" scored: "+this.score.toString());
+          this.clearService.clear();
+        }
+        clearInterval(countdown);
+      } else {
+        const minutes = Math.floor(totalSeconds / 60);
+        const seconds = totalSeconds % 60;
+        totalSeconds--;
+        this.timer=`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      }
+    }, 1000);
+  }
 
 }
 
